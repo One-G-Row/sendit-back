6123fd4dde0ef057fd44cc408a295605bb2e3f1a
alvinKyle <alvinkyle70@gmail.com>
2024-07-29 15:47:30 +0300

    tablename

--- server/models.py
+++ server/models.py
@@ -27,6 +27,8 @@ class Destination(db.Model):
 
 
 class Admin (db.Model):
+    __tablename__ = 'admins'
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50),nullable=False)
     last_name = db.Column(db.String(50),nullable=False)
