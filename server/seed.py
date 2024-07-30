9868f20daf560eaf563b8d0618a0da1c0cd33ef2
Charles <charlesmuteithia@gmail.com>
2024-07-30 12:03:28 +0300

    seeds the admins and destination data

--- server/seed.py
+++ server/seed.py
@@ -29,5 +29,24 @@ if __name__ == '__main__':
             users.append(user)
         db.session.add_all(users)
 
+                # Seed Admins
+        admins = [
+            Admin(first_name="Charles", last_name="Kagoko", email="chalokagoko@gmailcom", password_hash="charles123"),
+            Admin(first_name="Faith", last_name="Kimaru", email="faith@gmailcom", password_hash="faith123"),
+            Admin(first_name="Paul", last_name="Saitabau", email="paul@gmailcom", password_hash="paul123"),
+            Admin(first_name="Alvin", last_name="Kyle", email="alvin@gmailcom", password_hash="alvin123"),
+            Admin(first_name="Teddy", last_name="Kiplagat", email="teddy@gmailcom", password_hash="teddy123"),
+            Admin(first_name=fake.first_name(), last_name=fake.last_name(), email=fake.email(), password_hash="password123")
+        ]
+        db.session.add_all(admins)
+
+        # Seed Destinations
+        destinations = []
+        for _ in range(5):
+            destination = Destination(
+                location=fake.city(),
+                arrival_day=fake.date_tim)
+
+
 
 
