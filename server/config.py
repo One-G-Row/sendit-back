bfdd7974b01e294077b9cbeb9927dd7d9e8ed04b
alvinKyle <alvinkyle70@gmail.com>
2024-07-30 09:30:13 +0300

    updated

--- server/config.py
+++ server/config.py
@@ -2,18 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
 # Configuration for SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sendit.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'group 3'  # Change this to a strong secret key
 
 # Initialize extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+migrate = Migrate(app, db)
 
 # Import models to register them with SQLAlchemy
 from models import User, Admin, Parcel, Destination
 
-migrate = Migrate(app, db)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables if they don't exist
